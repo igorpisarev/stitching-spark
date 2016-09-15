afd88c6c2b5cdbe53ad020ed48e44d4d567ff479
Igor Pisarev <pisarevi@janelia.hhmi.org>
2016-09-15 18:01:25 -0400

    Configuration file converter: support multichannel datasets

--- TileConfigurationConverter.py
+++ TileConfigurationConverter.py
@@ -1,4 +1,5 @@
 import argparse
+import re
 import json
 
 if __name__ == '__main__':
@@ -12,24 +13,38 @@ if __name__ == '__main__':
 	try:
 		out_file = open( out_path, 'w' )
 	except IOError:
-		# non-writable path, store in the current directory as a fallback
+		# non-writable path, store the output file in the current directory as a fallback
 		out_path = out_path[sep_ind : ]
 		out_file = open( out_path, 'w' )
 	
-	data = []
-	index = 0
+	coords_pattern = re.compile('.*(\d{3}x_\d{3}y_\d{3}z).*');
+
+	groups = {}
+	tiles = []
 	with open(args.path) as in_file:
 		for line in in_file.readlines():
 			tokens = [t.strip() for t in line.strip().split()]
 			
-			#FIXME: temporary workaround for particular dataset
+			match = coords_pattern.match(tokens[0])
+			index =	len(tiles)
+
+			# workaround for datasets from betzigcollab
 			wrong_suffix = '_decon.tif'
 			if tokens[0].endswith(wrong_suffix):
 				tokens[0] = tokens[0][ : len(tokens[0]) - len(wrong_suffix) ] + '.tif'
 
-			data.append({ 'index': index, 'file': abs_path_prefix + tokens[0], 'position': [float(tokens[2]), float(tokens[1]), float(tokens[3])] })
+			# try to group by (x,y,z) coordinates obtained from filenames
+			if match:
+				grouping = match.group(1)
+			else:
+				grouping = index
 
-			index += 1
+			if not grouping in groups:
+				groups[grouping] = index
+				tiles.append({ 'index': index, 'position': [float(tokens[2]), float(tokens[1]), float(tokens[3])], 'channels':[] })
+			
+			tiles[ groups[grouping] ]['channels'].append(abs_path_prefix + tokens[0])
+
 	
-	out_file.write(json.dumps(data))
+	out_file.write(json.dumps(tiles))
 	out_file.close()
